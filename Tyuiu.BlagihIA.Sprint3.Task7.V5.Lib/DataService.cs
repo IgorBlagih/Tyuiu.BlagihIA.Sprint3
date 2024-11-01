b9ba311a376b07d3687ee6b20649142929ad9645
Игорь Благих <Tyuiu.Blagihia@outlook.com>
2024-11-01 20:31:53 +0500

    Task7

--- Tyuiu.BlagihIA.Sprint3.Task7.V5.Lib/DataService.cs
+++ Tyuiu.BlagihIA.Sprint3.Task7.V5.Lib/DataService.cs
@@ -14,7 +14,7 @@ namespace Tyuiu.BlagihIA.Sprint3.Task7.V5.Lib
             int count = 0;
             for (int x = startValue; x <= stopValue; x++)
             {
-                y = Math.Round(((2* Math.Sin(x))/(3*x+1.2))+Math.Cos(x)- 7 *x,2); 
+                y = Math.Round(((2 * (Math.Sin(x)))/(3*x+1.2))+Math.Cos(x)- 7 *x * 2,2); 
                 vArray[count] = y;
                 count++;
             }
